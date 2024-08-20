4e980bcf02b000951109e7a62c12cc351cd82ef8
chinmaydhabale <141849371+chinmaydhabale@users.noreply.github.com>
2024-08-20 10:30:36 +0530

    set env file for vite

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080/api/v1/data';
+const API_BASE_URL = import.meta.env.VITE_HOSTNAME;
 
 axios.defaults.withCredentials = true;
 
